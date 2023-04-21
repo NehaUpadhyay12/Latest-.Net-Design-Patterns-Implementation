d736fb61bd5326456e707aeafc2520b83f414055
NehaUpadhyay12 <upaneha1212@gmail.com>
2023-04-21 15:48:08 +0530

    Initial Commit

---
+++ FactoryDP/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SubjectFactory factory = new SubjectFactory();
+            Console.WriteLine(factory.GetSubject(0).SubjectName);
+        }
+    }
+}
---
+++ FactoryDP/SubjectFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDP
+{
+    internal class SubjectFactory
+    {
+        public Subject GetSubject(int id)
+        {
+            if(id==0)
+                return new PhysicsSubject();
+            else
+                return new ChemistrySubject();
+        }
+    }
+}
